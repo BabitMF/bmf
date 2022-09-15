5dad2a85a3c7adaa4fb914e776f52ac1d71d80d7
taoboyang <taoboyang@bytedance.com>
2022-09-15 14:53:20 +0800

    2022/9/15 decoder 增加注释

--- bmf/c_modules/src/ffmpeg_decoder.cpp
+++ bmf/c_modules/src/ffmpeg_decoder.cpp
@@ -176,6 +176,13 @@ CFFDecoder::CFFDecoder(int node_id, JsonParam option) {
     /** @addtogroup DecM
      * @{
      * @arg skip_frame: skip frame, exp. 32, make decoder discard processing depending on the option value, just as -skip_frame in ffmpeg commnad.
+     * AVDISCARD_NONE = -16, ///< discard nothing 
+     * AVDISCARD_DEFAULT = 0, ///< discard useless packets like 0 size packets in avi 
+     * AVDISCARD_NONREF = 8, ///< discard all non reference 
+     * AVDISCARD_BIDIR = 16, ///< discard all bidirectional frames 
+     * AVDISCARD_NONINTRA= 24, ///< discard all non intra frames 
+     * AVDISCARD_NONKEY = 32, ///< discard all frames except keyframes 
+     * AVDISCARD_ALL = 48, ///< discard all
      * @} */
     if (option.has_key("skip_frame")) {
         int tmp;

e3afcf9e0b21b421801af53eb489e0782fbb4bf0
Jie Fang <jief@nvidia.com>
2023-04-25 19:36:00 -0700

    add gpu transcode examples

---
+++ bmf/example/gpu_encode/test_gpu_encode.py
@@ -0,0 +1,124 @@
+import sys
+import time
+from multiprocessing import Process
+import multiprocessing
+from threading import Thread
+
+sys.path.append("../../")
+sys.path.append("../gpu_video_frame/")
+import bmf
+
+def test_gpu_encode():
+    #input_video_path = "../files/lark_stream0.flv"
+    input_video_path = "./test_video.mp4"
+    yuv_path = "./gpu_decode_result.yuv"
+    output_path = "gpu_encode_result.h264"
+
+    (
+        bmf.graph()
+            .decode({"input_path": input_video_path,
+                    "video_params": {
+                        "hwaccel": "cuda",
+                    }})["video"]
+           .module("cpu_gpu_trans_module", {"to_gpu": 0})
+           .encode(
+                None,
+                {
+                    "video_params": {
+                        "codec": "rawvideo",
+                    },
+                    "format": "rawvideo",
+                    "output_path": yuv_path
+                }
+            ).run()
+    )
+
+    (
+        bmf.graph()
+           .decode({"input_path": yuv_path,
+                    "video_params": {
+                        "vsync": 0,
+                    },
+                    "pix_fmt": "yuv420p",
+                    "s": "1280x720"
+                    })["video"]
+           .encode(
+                None,
+                {
+                    "video_params": {
+                        "codec": "h264_nvenc",
+                    },
+                    "output_path": output_path
+                }
+           ).run()
+    )
+    """
+        bmf.graph()
+           .decode({"input_path": yuv_path,
+                    "video_params": {
+                        "vsync": 0,
+                    },
+                    "pix_fmt": "yuv420p",
+                    "s": "1920x1080"
+                    })["video"]
+           .module("cpu_gpu_trans_module", {"to_gpu": 1})
+           .encode(
+                None,
+                {
+                    "video_params": {
+                        "codec": "h264_nvenc",
+                        "pix_fmt": "cuda",
+                    },
+                    "output_path": output_path
+                }
+           ).run()
+        """
+
+def task(num_frames):
+    yuv_path = "./gpu_decode_result.yuv"
+
+    (
+        bmf.graph()
+            .decode({"input_path": yuv_path,
+                    "video_params": {
+                        "vsync": 0,
+                    },
+                    "pix_fmt": "yuv420p",
+                    "s": "1280x720"
+                    })["video"]
+            .encode(
+                None,
+                {
+                    "video_params": {
+                        "codec": "h264_nvenc",
+                        "frames": num_frames,
+                    },
+                    "null_output": 1 
+                }
+           ).run()
+    )
+
+def test_gpu_encode_multi_thread_perf():
+    num_threads = 2
+    num_frames = 100
+    threads = []
+
+    for i in range(num_threads):
+        threads.append(Thread(target=task, args=(num_frames,)))
+
+    start = time.time()
+    for i in range(num_threads):
+        threads[i].start()
+    
+    for i in range(num_threads):
+        threads[i].join()
+    stop = time.time()
+    
+    duration = stop - start
+    total_frames = num_threads * num_frames
+
+    print("Total Frames Encoded={}, time={} seconds, FPS under multiple threads={}".format(total_frames, duration, total_frames / duration))
+
+if __name__ == "__main__":
+    test_gpu_encode()
+    test_gpu_encode_multi_thread_perf()
---
+++ bmf/example/gpu_transcode/test_gpu_transcode.py
@@ -0,0 +1,147 @@
+import sys
+import time
+from multiprocessing import Process
+import multiprocessing
+from threading import Thread
+
+sys.path.append("../../")
+sys.path.append("../gpu_video_frame/")
+sys.path.append("../gpu_module/")
+import bmf
+
+def test_gpu_transcode():
+    input_video_path = "../files/lark_stream0.flv"
+    output_video_path = "./gpu_transcoded_result.mp4"
+
+    graph = bmf.graph()
+
+    video = graph.decode({"input_path": input_video_path,
+                          "video_params": {
+                              "hwaccel": "cuda",
+                           }})
+    (
+        bmf.encode(
+            video["video"].module("scale_cvcuda", {"size": "1280x720"}),
+            video["audio"],
+            {
+                "output_path": output_video_path,
+                "video_params": {
+                    "codec": "h264_nvenc",
+                    #"pix_fmt": "cuda",
+                    "preset": "p6",
+                    "tune": "hq",
+                    "bit_rate": 5000000,
+                },
+            }
+        ).run()
+    )
+
+def test_gpu_transcode_1_to_n():
+    input_video_path = "../files/lark_stream0.flv"
+    output_video_path_1 = "./gpu_transcoded_result_1.mp4"
+    output_video_path_2 = "./gpu_transcoded_result_2.mp4"
+
+    graph = bmf.graph()
+
+    video = graph.decode({"input_path": input_video_path,
+                          "video_params": {
+                              "hwaccel": "cuda",
+                           }})
+    bmf.encode(
+        video["video"].module("scale_cvcuda", {"size": "1280x720"}),
+        video["audio"],
+        {
+            "output_path": output_video_path_1,
+            "video_params": {
+                "codec": "h264_nvenc",
+                #"pix_fmt": "cuda",
+                "preset": "p6",
+                "tune": "hq",
+                "bit_rate": 5000000,
+            },
+        }
+    )
+
+    bmf.encode(
+        video["video"].module("scale_cvcuda", {"size": "960x540"}),
+        video["audio"],
+        {
+            "output_path": output_video_path_2,
+            "video_params": {
+                "codec": "h264_nvenc",
+                #"pix_fmt": "cuda",
+                "preset": "p4",
+                "tune": "hq",
+                "bit_rate": 5000000,
+            },
+        }
+    )   
+
+    graph.run()
+
+def task():
+    input_video_path = "../files/perf_1080p_10k.h264"
+    graph = bmf.graph()
+
+    video = graph.decode({"input_path": input_video_path,
+                          "video_params": {
+                              "hwaccel": "cuda",
+                           }})
+    (
+        bmf.encode(
+            video["video"],
+            video["audio"],
+            {
+                "video_params": {
+                    "codec": "h264_nvenc",
+                    "pix_fmt": "cuda",
+                    "preset": "p3",
+                    "tune": "hq"
+                },
+                "null_output": 1 
+            }
+        ).run()
+    )
+
+def get_total_frames(input_video_path):
+    graph = bmf.graph()
+
+    frames = graph.decode({
+        "input_path": input_video_path,
+        "video_params": {
+            "hwaccel": "cuda",
+        }})["video"].start()
+    
+    num_frames = 0
+    for i in frames:
+        num_frames += 1
+    
+    return num_frames
+
+def test_gpu_transcode_multi_thread_perf():
+    input_video_path = "../files/perf_1080p_10k.h264"
+    num_threads = 2
+    threads = []
+
+    num_frames = get_total_frames(input_video_path)
+
+    for i in range(num_threads):
+        threads.append(Thread(target=task))
+    
+    start = time.time()
+    for i in range(num_threads):
+        threads[i].start()
+    
+    for i in range(num_threads):
+        threads[i].join()
+    stop = time.time()
+
+    duration = stop - start
+    total_frames = num_threads * num_frames
+    
+    print("Total Frames Transcoded={}, time={} seconds, FPS under multiple threads={}".format(total_frames, duration, total_frames / duration))
+
+if __name__ == "__main__":
+    test_gpu_transcode()
+    test_gpu_transcode_1_to_n()
+    test_gpu_transcode_multi_thread_perf()

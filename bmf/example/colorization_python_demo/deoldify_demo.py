a803be29c0151281c131cb5adf6b10168c84508c
Feng Wei <fengwei.frank19@bytedance.com>
2023-06-25 17:14:45 +0800

    Improve the colorization python demo

--- bmf/example/colorization_python_demo/deoldify_demo.py
+++ bmf/example/colorization_python_demo/deoldify_demo.py
@@ -3,18 +3,18 @@ import py_deoldify_module
 
 # Add the DeOldify folder to the python search path
 import sys
-sys.path.insert(0, '/content/DeOldify')
+sys.path.insert(0, './DeOldify')
 print(sys.path)
 
-input_video_path = '/content/drive/MyDrive/deoldify/oldvideo.mp4'
-output_video_path = '/content/colored_video.mp4'
-model_weight_path = '/content/DeOldify/'
+input_video_path = './DeOldify/test_videos/test_video.mp4'
+output_video_path = 'colored_video.mp4'
+model_weight_path = './DeOldify/'
 
 graph = bmf.graph()
 
 video = graph.decode({"input_path": input_video_path})
 
-output_video = video['video'].module('py_deoldify_module', option={"model_path": "/content/DeOldify/"})
+output_video = video['video'].module('py_deoldify_module', option={"model_path": model_weight_path})
 
 bmf.encode(
     output_video[0],

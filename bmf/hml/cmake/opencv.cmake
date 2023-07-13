aa8f463ed5e79f8429b0cff12283deb64bfa4c43
huheng.1989 <huheng.1989@bytedance.com>
2023-07-13 11:23:35 +0800

    backend convert enable: libtorch

--- bmf/hml/cmake/opencv.cmake
+++ bmf/hml/cmake/opencv.cmake
@@ -1,6 +1,6 @@
-
+#set(OpenCV_DIR "/usr/local/lib/cmake/opencv4")
 set(OpenCV_SHARED OFF)
-set(_CV_LIBS imgproc)
+set(_CV_LIBS imgproc core)
 if(HMP_ENABLE_CUDA)
     list(APPEND _CV_LIBS cudaimgproc cudawarping)
 endif()
--- bmf/sdk/cpp_sdk/include/bmf/sdk/torch_convertor.h
+++ bmf/sdk/cpp_sdk/include/bmf/sdk/torch_convertor.h
@@ -1,10 +1,71 @@
-#include <convert_backend.h>
 #include <hmp/torch/torch.h>
+#include <bmf/sdk/sdk_interface.h>
+#include <bmf/sdk/log.h>
+#include <bmf/sdk/convert_backend.h>
 
 namespace bmf_sdk {
 
-class Torch_Convertor : public Convertor
+template<>
+struct OpaqueDataInfo<at::Tensor>
 {
-}
+    const static int key = OpaqueDataKey::kATTensor;
+    static OpaqueData construct(const at::Tensor *tensor)
+    {
+        return std::make_shared<at::Tensor>(*tensor);
+    }
+
+};
+
+class TorchConvertor : public Convertor
+{
+public:
+    TorchConvertor(){}
+    int media_cvt(VideoFrame &src, const MediaDesc &dp) override {
+        try {
+            if (!src.frame().pix_info().is_rgbx()) {
+                BMFLOG(BMF_ERROR) << "tensor only support rgbx frame";
+                return -1;
+            }
+            auto tensor = src.frame().data()[0];
+            at::Tensor at = hmp::torch::tensor(tensor);
+            at::Tensor* pat = new at::Tensor(at);
+            src.private_attach<at::Tensor>(pat);
+
+        } catch (std::exception& e) {
+            BMFLOG(BMF_ERROR) << "convert to at::tensor err: " << e.what();
+            return -1;
+        }
+        return 0;
+    }
+
+    int media_cvt_to_videoframe(VideoFrame &src, const MediaDesc &dp) override {
+        try {
+            if (!dp.pixel_format.has_value()) {
+                BMFLOG(BMF_ERROR) << "VideoFrame format represented by the at::Tensor must be specified.";
+                return -1;
+            }
+
+
+            const at::Tensor *pat = src.private_get<at::Tensor>();
+            if (!pat) {
+                BMFLOG(BMF_ERROR) << "private data is null, please use private_attach before call this api";
+                return -1;
+            }
+            Tensor tensor = hmp::torch::from_tensor(*pat);
+            VideoFrame vf(Frame(tensor, hmp::PixelInfo(dp.pixel_format())));
+            vf.private_attach<at::Tensor>(pat);
+            src = vf;
+
+        } catch (std::exception& e) {
+            BMFLOG(BMF_ERROR) << "convert to cv::mat err: " << e.what();
+            return -1;
+        }
+        return 0;
+
+    }
+};
+
+static Convertor *torch_convert = new TorchConvertor();
+BMF_REGISTER_CONVERTOR(MediaType::kATTensor, torch_convert);
 
 }

e504a2a7236e86559aefeaa558d06ddd84914746
taoboyang <a510469540@vip.qq.com>
2023-12-13 22:33:39 +0800

    Fix mac ffmpeg version check

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -52,7 +52,6 @@ if(BMF_ENABLE_FFMPEG)
         set(BMF_FFMPEG_TARGETS 
             ffmpeg::avcodec ffmpeg::avformat ffmpeg::avfilter 
             ffmpeg::avdevice ffmpeg::avutil ffmpeg::swscale ffmpeg::swresample)
-        add_definitions(-DBMF_FFMPEG_VERSION=${BMF_FFMPEG_VERSION})
     else()
         set(BMF_FFMPEG_TARGETS)
         message(WARNING "FFMPEG libraries not found, disable it")

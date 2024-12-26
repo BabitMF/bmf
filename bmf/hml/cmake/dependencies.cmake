e6a024ec1fd9eab9b4341777af207025c47d9928
Jack Lau <2366536135@qq.com>
2024-12-26 21:57:53 +0800

    enable remote ssh connect to debug

--- bmf/hml/cmake/dependencies.cmake
+++ bmf/hml/cmake/dependencies.cmake
@@ -25,7 +25,7 @@ if(HMP_ENABLE_PYTHON)
         FetchContent_Declare(
             pybind11
             GIT_REPOSITORY https://github.com/pybind/pybind11.git
-            GIT_TAG 7331d381af4639f385fa07b5cb07d6c5dbacba71
+            GIT_TAG 2e0815278cb899b20870a67ca8205996ef47e70f
         )
         FetchContent_MakeAvailable(pybind11)
     else ()

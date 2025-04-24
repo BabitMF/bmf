c5b2174d031ed26a079f8ddc87cb9e80e29d39d4
taoboyang <taoboyang@bytedance.com>
2025-04-24 13:32:58 +0800

    Enable static integration of libuuid

--- cmake/FindLibuuid.cmake
+++ cmake/FindLibuuid.cmake
@@ -1,17 +1,22 @@
-find_path(Libuuid_INCLUDE_DIRS uuid/uuid.h)
-find_library(Libuuid_LIBRARIES uuid)
+set(Libuuid_INCLUDE_DIRS "")
+set(Libuuid_LIBRARIES "")
 
-if (Libuuid_LIBRARIES AND Libuuid_INCLUDE_DIRS)
+include(FetchContent)
+
+set(LIBUUID_STATIC ON CACHE BOOL "enable static build for libuuid" FORCE)
+set(LIBUUID_SHARED OFF CACHE BOOL "diable shared build for libuuid" FORCE)
+FetchContent_Declare(libuuid-cmake
+    GIT_REPOSITORY  https://github.com/gershnik/libuuid-cmake.git
+    GIT_TAG         v2.39.1
+    GIT_SHALLOW     TRUE
+)
+FetchContent_MakeAvailable(libuuid-cmake)
+if(NOT TARGET uuid_static)  # 通过目标存在性验证[7](@ref)
+    message(FATAL_ERROR "libuuid-cmake target fetch failed")
+else()
+    target_compile_options(uuid_static PRIVATE -fPIC)
+    set(Libuuid_LIBRARIES uuid::uuid)
+    get_target_property(Libuuid_INCLUDE_DIRS uuid_static INTERFACE_INCLUDE_DIRECTORIES)
     set(Libuuid_FOUND YES)
-    if (NOT Libuuid_FIND_QUIETLY)
-        message(STATUS "Found libuuid: ${Libuuid_LIBRARIES}")
-    endif ()
-else ()
-    if (Libuuid_FIND_REQUIRED)
-        message(SEND_ERROR "Could NOT find libuuid")
-    else ()
-        if (NOT Libuuid_FIND_QUIETLY)
-            message(STATUS "Could NOT find libuuid")
-        endif ()
-    endif ()
-endif ()
+endif()
+

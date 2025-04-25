a8a469cba71e77f7488e7bc26a6bf3c02c50632a
taoboyang <taoboyang@bytedance.com>
2025-04-26 00:02:26 +0800

    Add copy logic in uuid.h for libuuid-cmake

--- cmake/FindLibuuid.cmake
+++ cmake/FindLibuuid.cmake
@@ -11,12 +11,22 @@ FetchContent_Declare(libuuid-cmake
     GIT_SHALLOW     TRUE
 )
 FetchContent_MakeAvailable(libuuid-cmake)
-if(NOT TARGET uuid_static)  # 通过目标存在性验证[7](@ref)
+
+if(NOT TARGET uuid_static)
     message(FATAL_ERROR "libuuid-cmake target fetch failed")
 else()
     target_compile_options(uuid_static PRIVATE -fPIC)
     set(Libuuid_LIBRARIES uuid::uuid)
     get_target_property(Libuuid_INCLUDE_DIRS uuid_static INTERFACE_INCLUDE_DIRECTORIES)
+    if(NOT EXISTS ${Libuuid_INCLUDE_DIRS}/uuid/uuid.h)
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${FETCHCONTENT_BASE_DIR}/util-linux-src/libuuid/src/uuid.h  # 源文件路径
+                ${Libuuid_INCLUDE_DIRS}/uuid/uuid.h  # 目标路径
+            RESULT_VARIABLE copy_result
+            ERROR_VARIABLE copy_error
+        )
+    endif()
     set(Libuuid_FOUND YES)
 endif()
 

5c321c8fa8da962ef0df834bb96eed7ddb5e733d
Jonah van Praag <jonah.vanpraag@bytedance.com>
2025-07-25 17:12:04 +1000

    fix unclosed bracket

--- bmf/engine/connector/src/builder.cpp
+++ bmf/engine/connector/src/builder.cpp
@@ -444,6 +444,8 @@ void RealGraph::Start(
 
 int RealGraph::Close() {
     return graphInstance_->close();
+}
+
 int RealGraph::ForceClose() {
     return graphInstance_->force_close();
 }
@@ -822,6 +824,8 @@ void Graph::Start(std::vector<Stream> &generateStreams, bool dumpGraph,
 
 int Graph::Close() {
     return graph_->Close();
+}
+
 int Graph::ForceClose() {
     return graph_->ForceClose();
 }
